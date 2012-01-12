0117ebf544c17ace01b5ed10c0e0360225bd7c29
Jaybee- <jaybee@ego.gen.nz>
2012-01-13 09:46:21 +1300

    Make workaround for ciscos behave the same as atftp server

--- tftpserver.php
+++ tftpserver.php
@@ -791,17 +791,14 @@ class TFTPServer {
 
 	$rawexts = array_slice($a, 2, -1);
 
-	# Cisco IP Phone 7941 (and possibly others) return two nulls at the end
-	# Seems to be a breach of RFC rfc2347 ( http://tools.ietf.org/html/rfc2347 ) 
-	# This is a workaround - if there is exactly half an extension (i.e. key,
-	# but no value), and the key is empty, then the half-extension is dropped.
-	if ((count($rawexts)==1) && ($rawexts[0]=="")) {
-		$rawexts=array();
-	}
+	# Cisco IP Phone 7941 (and possibly others) return an extra null
+	# at the end; a breach of RFC rfc2347. This is a workaround
 	if(count($rawexts) % 2 != 0) {
-	  $this->log_warning($peer, "request: malformed extension " .
-			     "key/value pairs " . TFTPOpcode::name($op));
-	  return false;
+	  if (array_pop($rawexts)!="") {
+	    $this->log_warning($peer, "request: malformed extension " .
+			       "key/value pairs " . TFTPOpcode::name($op));
+	    return false;
+	  }
 	}
 
 	$extensions = array();
